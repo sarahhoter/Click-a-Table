04358d5c5782af22c13820cf14d9e6913a107acc
sarah-hoter <sarah.hoter@gmail.com>
2016-03-22 13:02:53 +0200

    added js files

---
+++ js/app.js
@@ -0,0 +1,37 @@
+﻿/// <reference path="angular.js" />
+
+var app = angular.module("myApp", ["ngRoute"]);
+
+app.controller("LoginController", ["$scope", LoginController]);
+app.controller("OrderController", ["$scope", OrderController]);
+app.controller("MenuController", ["$scope", MenuController]);
+app.controller("CourseTypeController", ["$scope", CourseTypeController]);
+app.controller("CourseController", ["$scope", CourseController]);
+
+app.config(['$routeProvider',
+function ($routeProvider) {
+    $routeProvider.
+        when('/login', {
+            templateUrl: 'login.html',
+            controller: 'LoginController'
+        }).
+        when('/order', {
+            templateUrl: 'order.html',
+            controller: 'OrderController'
+        }).
+        when('/menu', {
+            templateUrl: 'menu.html',
+            controller: 'MenuController'
+        }).
+        when('/courseType', {
+            templateUrl: 'courseType.html',
+            controller: 'CourseTypeController'
+        }).
+        when('/course', {
+            templateUrl: 'course.html',
+            controller: 'CourseController'
+        }).
+        otherwise({
+            redirectTo: '/index'
+        });
+}]);
