da553961dd50414aba376a7ea9b41aef86731dad
sarah-hoter <sarah.hoter@gmail.com>
2016-03-23 08:33:51 +0200

    Create headerDirective.js

---
+++ directives/headerDirective.js
@@ -0,0 +1,23 @@
+/// <reference path="../js/angular.js" />
+app.directive('header', function () {
+    return {
+
+        restrict: 'E',
+        templateUrl: "header.html",
+        scope: {
+            items: "="
+        },
+        controller: ["$scope", function ($scope) {
+
+            $scope.setActive = function (index) {
+
+                angular.forEach($scope.items, function (item) {
+                    item.isActive = false;
+                });
+
+                $scope.items[index].isActive = true;
+            }
+
+        }]
+    }
+});
