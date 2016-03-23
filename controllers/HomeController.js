cd5d861133bb74d935a1a3229ca20de7f4265058
sarah-hoter <sarah.hoter@gmail.com>
2016-03-23 09:06:33 +0200

    Added files via upload

---
+++ controllers/HomeController.js
@@ -0,0 +1,10 @@
+﻿function HomeController($scope) {
+    $scope.items = [
+        { label: "Home", path: "#", imageSrc: "", isActive: true },
+        { label: "login", path: "#/login", imageSrc: "", isActive: false },
+        { label: "menu", path: "#/menu", imageSrc: "", isActive: false },
+        { label: "course type", path: "#/courseType", imageSrc: "", isActive: false },
+        { label: "course", path: "#/course", imageSrc: "", isActive: false },
+        { label: "order", path: "#/order", imageSrc: "", isActive: false }
+    ];
+}
