34c9934d6d8ab6cf54c5d54ff72d4d0f6469ea1b
sarah-hoter <sarah.hoter@gmail.com>
2016-05-16 08:32:35 +0300

    added server pages, moved client pages into app folder
    added server.js, services and controllers folder

---
+++ services/menu.service.js
@@ -0,0 +1,32 @@
+﻿var config = require('config.json');
+var mongoose = require('mongoose'); //get DB
+var Schema = mongoose.Schema; //to create Schema
+var connection = mongoose.createConnection(config.connectionString);//connect to the db server
+//Schema = document
+var menu = new Schema({
+    name:  String,
+    sureName: String
+});
+
+//model = collection
+var Menu = connection.model('Menu', menu);
+
+var service = {};
+
+service.getMenu = getMenu;
+
+module.exports = service;
+
+function getMenu() {
+    //find all menu items in db
+    Menu.find({}, function(err, menu) {
+        var menuMap = {};//return object
+
+        //fill up the object
+        menu.forEach(function(item) {
+            menuMap[item._id] = item;
+        });
+        //return the menu object
+        return JSON.stringify(menuMap, null, "\t");
+    });
+}
