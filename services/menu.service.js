ebc23c21bb00a4e4fb3ddaa2646892f6c36d9ba5
sarah-hoter <sarah.hoter@gmail.com>
2016-05-18 15:31:46 +0300

    Revert "arrangement"
    This reverts commit 494e90eb06363375c5bc0074f9d92aba7e962469.

---
+++ services/menu.service.js
@@ -0,0 +1,34 @@
+﻿var config = require('config.json');
+var mongoose = require('mongoose'); //get DB
+var Schema = mongoose.Schema; //to create Schema
+var connection = mongoose.createConnection(config.connectionString);//connect to the db server
+//Schema = document
+var menu = new Schema({
+    id:  Number,
+    name: String,
+    image: String,
+    parentId: Number
+});
+
+//model = collection
+var Menu = connection.model('Menu', menu);
+
+var service = {};
+
+service.getMenu = getMenu;
+
+module.exports = service;
+
+function getMenu() {
+    //find all menu items in db
+    Menu.find({}, function(err, menu) {
+        var menuMap = {};//return object
+
+        //fill up the object
+        menu.forEach(function(item) {
+            menuMap[item._id] = item;
+        });
+        //return the menu object
+        return JSON.stringify(menuMap, null, "\t");
+    });
+}
