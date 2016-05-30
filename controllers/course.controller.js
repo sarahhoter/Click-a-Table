50e42454b54ca69abbfe8818e0b18be2becd9738
ChagitY <chagit1234@gmail.com>
2016-05-30 12:48:45 +0300

    update course.controller.js (#54)
    :+1:

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -8,6 +8,7 @@ var connection = mongoose.createConnection(config.connectionString);//connect to
 router.post('/init', initData);
 router.get('/', getAll);
 router.get('/:courseTypeId', getByCourseItem);
+router.get('/details/:courseId', getByCourseId);
 
 module.exports = router;
 
@@ -15,7 +16,7 @@ var courseSchema = require('../models/course.model');
 var Course = connection.model('Course', courseSchema);
 
 function initData() {
-    Course.update({courseId: 3}, {courseTypeId: 1, label: "עוף3", image: "food1.jpg"},
+    Course.update({courseId: 3}, {courseTypeId: 1, label: "עוף3", image: "food1.jpg",businessPrice:"30",price:"40"},
         {upsert: true}, //insert if not exists
         function(err, numAffected) {
             if (err)
@@ -23,7 +24,7 @@ function initData() {
         }
     );
 
-    Course.update({courseId: 4}, {courseTypeId: 1, label: "עוף1", image: "food2.jpg"},
+    Course.update({courseId: 4}, {courseTypeId: 1, label: "עוף1", image: "food2.jpg",businessPrice:"40",price:"50"},
         {upsert: true}, //insert if not exists
         function(err, numAffected) {
             if (err)
@@ -31,7 +32,7 @@ function initData() {
         }
     );
 
-    Course.update({courseId: 5}, {courseTypeId: 2, label: "סושי", image: "food2.jpg"},
+    Course.update({courseId: 5}, {courseTypeId: 2, label: "סושי", image: "food2.jpg",businessPrice:"50",price:"60"},
         {upsert: true}, //insert if not exists
         function(err, numAffected) {
             if (err)
@@ -55,6 +56,19 @@ function getByCourseItem(req, res) {
         getAll(req, res);
     else {
         Course.find({courseTypeId: req.params.courseTypeId}, function (err, courses) {
+            if (err)
+                res.send(err);
+
+            res.json(courses); // return all courses in JSON format
+        });
+    }
+}
+
+
+function getByCourseId(req, res) {
+    if (req.params.courseId != "0") //TODO for init - maybe should find another way
+    {
+        Course.find({id: req.params.courseId}, function (err, courses) {
             if (err)
                 res.send(err);
 
